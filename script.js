e0922b32da59916a4345a381a21a59f9a0fdd4d3
alex ng <alexng@aang.local>
2017-03-17 23:13:05 -0400

    initial version with geolocation

---
+++ script.js
@@ -0,0 +1,41 @@
+var position = {
+  'latitude' : 0,
+  'longitude' : 0
+}
+
+function geoLocate()
+{
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(storePosition);
+  }
+  else
+  {
+    alert('could not determinate');
+  }
+}
+
+function storePosition(position)
+{
+  this.position.latitude = position.coords.latitude;
+  this.position.longitude = position.coords.longitude;
+}
+
+function initMap() 
+{
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), 
+        {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
+
+function log(tag, message)
+{
+  console.log('['+tag+']'+message);
+}
