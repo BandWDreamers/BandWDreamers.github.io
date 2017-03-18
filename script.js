7e387fb5b3ae56bfe2a8b9fe996f5e2c7c7fba16
alex ng <alexng@aang.local>
2017-03-17 23:40:12 -0400

    fixed map view

--- script.js
+++ script.js
@@ -25,14 +25,8 @@ function initMap()
 {
   log('initMap', 'start')
   var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
+  var map = new google.maps.Map(document.getElementById("map"), { zoom : 7, center : uluru });
+  var marker = new google.maps.Marker({ position : uluru, map : map });
   log('initMap','end');
 }
 
