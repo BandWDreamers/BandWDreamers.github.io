762dcd97c817357dbc90c45a8144b2bebfb26dca
alex ng <alexng@aang.local>
2017-03-18 00:34:30 -0400

    geolocation now displays on map view

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var position = {
-  'latitude' : 0,
-  'longitude' : 0
+  'latitude' : -25.363,
+  'longitude' : 131.044
 }
 
 function geoLocate()
@@ -19,12 +19,13 @@ function storePosition(position)
 {
   this.position.latitude = position.coords.latitude;
   this.position.longitude = position.coords.longitude;
+  initMap();
 }
 
 function initMap() 
 {
   log('initMap', 'start')
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: position.latitude, lng: position.longitude};
   var map = new google.maps.Map(document.getElementById("map"), { zoom : 7, center : uluru });
   var marker = new google.maps.Marker({ position : uluru, map : map });
   log('initMap','end');
